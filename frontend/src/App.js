50a859605c1d523cf5cb1c2497045131d12ebb6c
juakiCarbonell <joachim.carbonell@googlemail.com>
2020-10-02 16:32:39 +0200

    main layout added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 const App = () => {
   return (
-    <>
-      Welcome to PRoshop
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" exact component={Home} />
+          <Route path="/product/:id" exact component={Product} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
