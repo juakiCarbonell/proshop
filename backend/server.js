0e96cf5651639641d5c41412966336df7fc0f3d6
juakiCarbonell <joachim.carbonell@googlemail.com>
2020-10-05 18:51:23 +0200

    products backend

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,32 @@
-import express from "express"
-import dotenv from "dotenv"
-import products from "./data/products.js"
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middelware/errorMiddleware.js";
+
+import productRoutes from "./routes/products.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("api is rundfdning");
-});
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
+  res.send("api is running");
 });
 
-const PORT = process.env.PORT || 5000
+app.use("/api/products", productRoutes);
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.use(notFound);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
